920923d97335fa2aadbfb5615f5418b3ad2c35ed
Dennys Winslow De Menezes Arantes <dennys.arantes@tse.jus.br>
2023-12-05 19:58:49 -0300

    configuração da aula 2 com pressable ao invés do botão utilizado na aula

---
+++ src/telas/Servicos/Item/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+import {Pressable, TouchableOpacity, View, Text } from 'react-native';
+
+import CampoInteiro from '../../../componentes/CampoInteiro';
+import Botao from '../../../componentes/Botao';
+
+import estilos from './estilo';
+
+export default function Item({nome, preco, descricao}) {
+
+  const [quantidade, setQuantidade] = useState(1);
+  const [total, setTotal] = useState(preco);
+  const [expandir, setExpandir] = useState(false);
+
+  const atualizaQuantidadeTotal = (novaQuantidade) => {
+    setQuantidade(novaQuantidade);
+    calculaTotal(novaQuantidade)
+  }
+
+  const calculaTotal = (novaQuantidade) => {
+    setTotal(novaQuantidade * preco);
+  }
+
+  const inverteExpandir = () => {
+    setExpandir(!expandir);
+    atualizaQuantidadeTotal(1);
+  }
+
+  return <>
+    <Pressable 
+      android_ripple={{color: 'gray', borderless: false}} 
+       style={estilos.informacao} onPress={inverteExpandir}>
+      <Text style={estilos.nome}>{ nome }</Text>
+      <Text style={estilos.descricao}>{ descricao }</Text>
+      <Text style={estilos.preco}>{ 
+        Intl.NumberFormat('pt-BR', {
+          style: 'currency', currency: 'BRL'
+        }).format(preco)
+      }</Text>
+    </Pressable >
+    {expandir &&
+      <View style={estilos.carrinho}>
+        <View>
+          <View style={estilos.valor}>
+            <Text style={estilos.descricao}>Quantidade:</Text>
+            <CampoInteiro estilos={estilos.quantidade} valor={quantidade} acao={atualizaQuantidadeTotal} />
+          </View>
+          <View style={estilos.valor}>
+            <Text style={estilos.descricao}>Total:</Text>
+            <Text style={estilos.preco}>{
+              Intl.NumberFormat('pt-BR', {
+                style: 'currency', currency: 'BRL'
+              }).format(total)
+            }</Text>
+          </View>
+        </View>
+        <Botao valor="Adicionar" acao={() => {}} />
+      </View>
+    }
+    <View style={estilos.divisor} />
+  </>
+}
